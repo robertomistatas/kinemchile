3a4af3ef5b8b7618702eb4233a9eaf71e97a2e9c
robertomistatas <roberto@mistatas.com>
2025-04-25 17:33:27 -0400

    feat: configuración inicial del proyecto con Next.js y Firebase

--- next.config.mjs
+++ next.config.mjs
@@ -13,38 +13,14 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-}
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle the undici package
+    config.module.rules.push({
+      test: /node_modules\/undici\/.*\.js$/,
+      type: 'javascript/auto',
+    });
 
-if (userConfig) {
-  // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig
-
-  for (const key in config) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...config[key],
-      }
-    } else {
-      nextConfig[key] = config[key]
-    }
+    return config;
   }
 }
 
